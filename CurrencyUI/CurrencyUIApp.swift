74246bb6c194232c55b1e1f53237b5dc0dc97266
kamilksi <kamilksiazek002@gmail.com>
2022-08-31 22:27:15 +0200

    Initial commit

---
+++ CurrencyUI/CurrencyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyUIApp.swift
+//  CurrencyUI
+//
+//  Created by Kamil Książek on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
